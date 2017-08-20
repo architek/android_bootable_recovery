0ccd50fb16d4d3508971abf7f35a9942a7929470
Jack <teebeenator@gmail.com>
2017-08-20 09:34:44 +0200

    No libz required
    Change-Id: I136e93ea05ddf0af945de03eb23912ed4f7a2763

--- memtester/Android.mk
+++ memtester/Android.mk
@@ -8,14 +8,7 @@ LOCAL_MODULE_CLASS := RECOVERY_EXECUTABLES
 LOCAL_MODULE_PATH := $(TARGET_RECOVERY_ROOT_OUT)/sbin
 LOCAL_CFLAGS :=
 LOCAL_SRC_FILES = memtester.c tests.c
-LOCAL_C_INCLUDES += $(LOCAL_PATH) \
-					external/zlib
-LOCAL_SHARED_LIBRARIES += libz libc
-
-#LOCAL_POST_INSTALL_CMD := \
-#    $(hide) mkdir -p $(TARGET_RECOVERY_ROOT_OUT)/sbin && \
-#    ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/gunzip && \
-#    ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/gzip && \
-#    ln -sf pigz $(TARGET_RECOVERY_ROOT_OUT)/sbin/unpigz
+LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_SHARED_LIBRARIES += libc
 
 include $(BUILD_EXECUTABLE)
